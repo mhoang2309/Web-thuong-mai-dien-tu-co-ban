5fc5525265bbc281f4e7236197159a5cc362ed73
Hoàng Nguyễn <mhoang1998@gmail.com>
2021-06-14 14:53:25 +0700

    first commit

---
+++ data/products.js
@@ -0,0 +1,47 @@
+var list_products = [{
+    "name": "Toandaiso",
+    "company": "Toán đại số",
+    "img": "img/products/toandaiso.jpg",
+    "price": "1.990.000",
+    "star": 5,
+    "rateCount": 26,
+    "promo": {
+        "name": "",
+        "value": "0"
+    },
+    "detail": {
+        "gv": "Võ Đăng Khoa",
+        "trinhdo": "Thạc sĩ",
+        "kinhnghiem": "10 năm",
+        "time": "45 phút/tiết",
+        "tuan": "3 buổi/tuần",
+        "diadiem": "Google rom",
+        "thoigian": "25 tiết",
+        "chuthich": ""
+    },
+    "masp": "Toands"
+},
+{
+    "name": "Toanhinh",
+    "company": "Hinh Học",
+    "img": "img/products/toanhinh.jpg",
+    "price": "990.000",
+    "star": 4.5,
+    "rateCount": 26,
+    "promo": {
+        "name": "",
+        "value": "0"
+    },
+    "detail": {
+        "gv": "Võ Đăng Khoa",
+        "trinhdo": "Thạc sĩ",
+        "kinhnghiem": "10 năm",
+        "time": "45 phút/tiết",
+        "tuan": "3 buổi/tuần",
+        "diadiem": "Google rom",
+        "thoigian": "25 tiết",
+        "chuthich": ""
+    },
+    "masp": "Toanhinh"
+}
+]
